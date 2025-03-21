b3c255eeca98b32695be677e0f586f1c7b2574f4
Jonathan Dantas Nobrega <rm91889@estudante.fieb.edu.br>
2025-03-21 10:02:44 -0300

    porta

--- Assets/Script/ControllerLabirinto.cs
+++ Assets/Script/ControllerLabirinto.cs
@@ -5,28 +5,25 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class ControllerLabirinto : MonoBehaviour
 {
-    public Rigidbody2D rig;
+   
     public BoxCollider2D box;
     // Start is called before the first frame update
     void Awake()
     {
-        rig = GetComponent<Rigidbody2D>();
+        
         box = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
-    // void OnCollisionEnter(Collision other)
-    // {
-    //     if(other.Compare.Tag("Player"))
-    //     {
-    //         box.isTrigger = false;
-    //     }
-    // }
-
-
-
-
-
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            {
+                box.isTrigger = true;
+            }
+        }
+    }
 
 
 
