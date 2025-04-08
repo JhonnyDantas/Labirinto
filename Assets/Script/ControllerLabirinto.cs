1df45c6d5a4ef0462012465e3893c06f16968f3d
Jonathan Dantas Nobrega <rm91889@estudante.fieb.edu.br>
2025-04-08 11:09:35 -0300

    Labirinto

--- Assets/Script/ControllerLabirinto.cs
+++ Assets/Script/ControllerLabirinto.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider2D))]
+// [RequireComponent(typeof(BoxCollider2D))]
 public class ControllerLabirinto : MonoBehaviour
 {
     [SerializeField]
